7decf6875958001fbf5aef828e7545b190093c1e
Alesya <palesyasya@gmail.com>
2023-10-20 22:45:16 +0200

    registration

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import Authorization from './authentification/authorization';
 
 const app = express();
 const PORT = 5000;
 const authorization = new Authorization();
 
-app.use(bodyParser.json());
-
-app.post('/register', authorization.registerUser());
-app.post('/login', authorization.loginUser());
+app.post('/register', authorization.registerUser);
+app.post('/login', authorization.loginUser);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
