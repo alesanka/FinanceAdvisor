7f9edc4846ce2cef5f16aea727383d25642ddbcf
Alesya <palesyasya@gmail.com>
2023-11-06 02:32:09 +0100

    check by role

--- src/middlewere/checkRole.js
+++ src/middlewere/checkRole.js
@@ -2,7 +2,8 @@ import { userModel } from '../services/userModel.js';
 
 const checkUserRole = (roles) => async (req, res, next) => {
   try {
-    const { user_id } = req;
+    const { user_id } = req.body;
+    console.log(user_id);
     if (!user_id) {
       throw new Error('User id is required for checking role.');
     }
