3efd3a0f2bd5c2673a30f80e5167574b2da5bf6c
Alesya <palesyasya@gmail.com>
2023-11-12 15:36:58 +0100

    new test

--- tests/testControllers.js
+++ tests/testControllers.js
@@ -1,32 +1,27 @@
-// import { documentController } from '../path/to/DocumentController';
-// import { documentModel } from '../path/to/models/documentModel';
+import { documentController } from '../path/to/DocumentController';
+import { documentModel } from '../path/to/models/documentModel';
 
-// jest.mock('../path/to/models/documentModel');
+jest.mock('../path/to/models/documentModel');
 
-// describe('DocumentController', () => {
-//   describe('createDocument', () => {
-//     it('should create a document and send a response', async () => {
-//       // Настройка
-//       const req = {
-//         body: {/* данные документа */},
-//       };
-//       const res = {
-//         status: jest.fn().mockReturnThis(),
-//         send: jest.fn(),
-//       };
-//       documentModel.createDocument.mockResolvedValue('docId');
+describe('DocumentController', () => {
+  describe('createDocument', () => {
+    it('should create a document and send a response', async () => {
+      const req = {
+        body: {},
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn(),
+      };
+      documentModel.createDocument.mockResolvedValue('docId');
 
-//       // Действие
-//       await documentController.createDocument(req, res);
+      await documentController.createDocument(req, res);
 
-//       // Проверка
-//       expect(documentModel.createDocument).toHaveBeenCalledWith(req.body);
-//       expect(res.status).toHaveBeenCalledWith(201);
-//       expect(res.send).toHaveBeenCalledWith('Document was added successfully. Document id - docId');
-//     });
-
-//     // Другие тесты для обработки ошибок и других сценариев...
-//   });
-
-//   // Тесты для других методов...
-// });
+      expect(documentModel.createDocument).toHaveBeenCalledWith(req.body);
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.send).toHaveBeenCalledWith(
+        'Document was added successfully. Document id - docId'
+      );
+    });
+  });
+});
